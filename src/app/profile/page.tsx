c3fc2cbea805069a8fc959591ae67ef3307797a2
Avinash Kokare <avinash.kokare@fitcircle.in>
2023-07-25 23:18:04 +0530

    created User model

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen">Profile</div>
+  )
+}
+
+export default Profile;
---
+++ src/models/userModal.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    isUserVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+});
+
+const User = mongoose.model.users || mongoose.model('User', UserSchema);
+
+export default User;
